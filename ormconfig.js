e8957881a0ce864d426d479ffda952bb8ce01346
igordev2 <igorsouzabezerra@outlook.com>
2022-01-20 18:35:24 -0300

    run migrations in docker container

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'mysql',
-  host: 'localhost',
+  host: process.env.MYSQL_HOST,
   port: Number(process.env.MYSQL_PORT),
   username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
