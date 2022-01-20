5941b0b70bca6457a87bd95ee2542ea729d5246e
igordev2 <igorsouzabezerra@outlook.com>
2022-01-20 18:12:47 -0300

    connection to run migrations

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,14 @@
 module.exports = {
   type: 'mysql',
-  host: process.env.MYSQL_HOST,
+  host: 'localhost',
   port: Number(process.env.MYSQL_PORT),
   username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  entities: [__dirname + '/**/*.entity{.js,.ts}'],
+  entities: ['./src/**/*.entity.ts'],
   synchronize: false,
-  migrations: ['src/infra/typeorm/migrations'],
+  migrations: ['./src/infra/typeorm/migrations/*ts'],
   cli: {
-    migrationsDir: 'src/infra/typeorm/migrations',
+    migrationsDir: './src/infra/typeorm/migrations',
   },
 };
