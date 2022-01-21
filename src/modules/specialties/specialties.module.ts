cab8b6ecd7491d378b09ec5651a865d2bcd689b2
igordev2 <igorsouzabezerra@outlook.com>
2022-01-20 21:42:46 -0300

    replace dot with dash

--- src/modules/specialties/specialties.module.ts
+++ src/modules/specialties/specialties.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Specialty } from './entities/specialty.entity';
 import { SpecialtiesRepository } from './repository/specialties.repository';
-import { ListSpecialtiesController } from './usecases/list/list.specialties.controller';
-import { ListSpecialtiesUseCase } from './usecases/list/list.specialties.usecase';
+import { ListSpecialtiesController } from './usecases/list/list-specialties.controller';
+import { ListSpecialtiesUseCase } from './usecases/list/list-specialties.usecase';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Specialty, SpecialtiesRepository])],
