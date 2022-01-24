7f91cd44a5a2198ec848993f7bbf822ba39dfe11
igordev2 <igorsouzabezerra@outlook.com>
2022-01-23 22:12:16 -0300

    list specialties - test e2e

--- test/specialties/list-specialties.e2e-spec.ts
+++ test/specialties/list-specialties.e2e-spec.ts
@@ -1,24 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { SpecialtiesModule } from '../../src/modules/specialties/specialties.module';
+// import { SpecialtiesModule } from '../../src/modules/specialties/specialties.module';
+import { AppModule } from '../../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [SpecialtiesModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/api/v1/specialties (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/api/v1/specialties')
-      .expect(200)
-      .expect([]);
+  it('(GET) -> /api/v1/specialties ', async () => {
+    const response = await request(app.getHttpServer()).get(
+      '/api/v1/specialties',
+    );
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0]).toHaveProperty('id');
+    expect(response.body[0]).toHaveProperty('description');
+    expect(response.body[0]).toHaveProperty('createdAt');
+    expect(response.body[0]).toHaveProperty('deletedAt');
   });
 });
